1a77b68c0dde8801c8677ec858c893eb9884dc43
Iván Carrascosa Barragán <ivan.carrascosa@info2>
2025-11-03 14:42:48 +0100

    ejercicios tema 8

---
+++ Unidad8/Formulario/Formulario/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using Formulario.Models;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace Formulario.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Persona persona)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Saludo(String nombre)
+        {
+            @ViewBag.nombre = nombre;
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Unidad8/Formulario/Formulario/Models/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Persona
+    {
+        #region atributos privados
+        private int _id;
+        private string _nombre;
+        private string _apellido;
+
+        #endregion
+        #region getters y setters
+        public int id { get { return _id; } }
+        public string nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        public string apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+        #endregion
+        #region constructores
+        public Persona() { }
+        public Persona(int id, string nombre, string apellido)
+        {
+            _id = id;
+            _nombre = nombre;
+            _apellido = apellido;
+        }
+
+        #endregion
+    }
+}
